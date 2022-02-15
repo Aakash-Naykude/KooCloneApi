552442bee7604ea2f8e48da4ce697a11f046aaf8
Aakash-Naykude <aakashnaykude123@gmail.com>
2022-02-15 18:35:09 +0530

    make it better

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
-
+app.use(cors())
 const userController = require("./controllers/user.controller");
 app.use("/user", userController);
 
